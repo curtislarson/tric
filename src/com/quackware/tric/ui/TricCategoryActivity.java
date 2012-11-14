55947f67111ff77f61f90fbf0641c88f7064033f
quackware <quackware@gmail.com>
2012-11-14 00:15:51 -0500

    Messing around with layouts to attempt to get some good looking buttons

--- src/com/quackware/tric/ui/TricCategoryActivity.java
+++ src/com/quackware/tric/ui/TricCategoryActivity.java
@@ -8,6 +8,7 @@ import com.quackware.tric.stats.SocialStats.SocialStats;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
